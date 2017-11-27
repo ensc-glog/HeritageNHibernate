ce36cb84cde9ca47e2f7daa300f66093b286d656
bpesquet <bpesquet@gmail.com>
2017-11-27 14:37:00 +0100

    Table per hierarchy

---
+++ TablePerHierarchy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace TablePerHierarchy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ISessionFactory sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            ISession session = sessionFactory.OpenSession();
+
+            Console.WriteLine("Liste des personnages :");
+            IList<Character> characters = session.Query<Character>().ToList();
+            foreach (Character character in characters)
+                Console.WriteLine(character);
+        }
+    }
+}
---
+++ TablePerHierarchy/database.sql
@@ -0,0 +1,21 @@
+﻿
+create database if not exists heritagenhib_hierarchy character set utf8 collate utf8_unicode_ci;
+use heritagenhib_hierarchy;
+
+grant all privileges on heritagenhib_hierarchy.* to 'hnh_user'@'localhost' identified by 'secret';
+
+drop table if exists t_character;
+
+create table t_character (
+    char_id integer not null primary key auto_increment,
+    char_type varchar(20) not null,
+    char_name varchar(100) not null,
+    char_title varchar(100),
+    char_xpvalue integer
+);
+
+insert into t_character values (1, 'PLA', 'Aurora', 'Knight', null);
+insert into t_character values (2, 'PLA', 'Glacius', 'King', null);
+
+insert into t_character values (3, 'NPC', 'ZogZog', null, 250);
+insert into t_character values (4, 'NPC', 'Onyxia', null, 4000);
